057c5dacdbd06645ee7180c81da0e5bead5102d1
shawon13 <rakibarg1519@gmail.com>
2023-10-24 22:33:17 +0600

    added the header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css'
+import logo from '../../images/Logo.svg'
+const Header = () => {
+    return (
+        <nav className='header'>
+            <div>
+                <img src={logo} alt="" />
+            </div>
+            <div className='nav'>
+                <a href="/order">Order</a>
+                <a href="/review">Order Review</a>
+                <a href="/inventory">Manage Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
